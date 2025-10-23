7526a77ad530ccec81e6b04eea4f5f452a7e60bb
YexuanXiao <bizwen@nykz.org>
2025-10-23 21:35:35 +0800

    Spelling

--- src/modules/LightSwitch/LightSwitchCommon/ThemeHelper.h
+++ src/modules/LightSwitch/LightSwitchCommon/ThemeHelper.h
@@ -4,5 +4,5 @@ void SetSystemTheme(bool dark);
 void SetAppsTheme(bool dark);
 bool GetCurrentSystemTheme();
 bool GetCurrentAppsTheme();
-// Returned 0 indicates success; otherwise, the reason is returned, see definatiion
+// Returned 0 indicates success; otherwise, the reason is returned, see definition
 int SetWallpaper(std::wstring const& wallpaperPath, int style) noexcept;
